1d4fa3787a6b66c0c0941934a5864a0385c9161d
dnztrmn918 <dnztrmnn@icloud.com>
2025-08-06 23:39:38 +0300

    Update skip.py

--- ShrutiMusic/plugins/admins/skip.py
+++ ShrutiMusic/plugins/admins/skip.py
@@ -36,7 +36,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
+    filters.command(["skip", "cskip", "next", "cnext", "geç"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
